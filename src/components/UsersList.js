16885af13ed7f1fa18612b03d40a0776b29f555f
LombardiniJere <jere.lombardini@gmail.com>
2023-08-29 08:54:38 -0300

    useThunk & hooks

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,29 +1,55 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchUsers } from "../store/thunks/fetchUsers";
+import { useSelector } from "react-redux";
+import { fetchUsers, addUser } from "../store";
+import { useThunk } from "../hooks/use-thunk";
+import Button from "./Button";
 import Skeleton from "./Skeleton";
 
-function UserList () {
-  const dispatch = useDispatch();
-  const { isLoading, data, error } = useSelector((state) => {
+function UserList() {
+  const [ doFetchUsers, isLoadingUsers, loadingUsersError ] = useThunk(fetchUsers);
+  const [ doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+  const { data } = useSelector((state) => {
     return state.users;
   });
 
   useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
+    doFetchUsers();
+  },[doFetchUsers]);
+
+  const handleAddUser = () => {
+    doCreateUser();
+  };
 
   // times={6} : number of gray color bars loading
-  if (isLoading) {
+  if (isLoadingUsers) {
     return <Skeleton times={6} className='h-10 w-full' />
   };
-  if (error) {
+  if (loadingUsersError) {
     return <div>Error fetching data...</div>;
   };
 
+  const renderedUsers = data.map((user) => {
+    return <div key={user.id} className="mb-2 border rounded">
+      <div className="flex p-2 justify-between items-center cursor-pointer">
+        {user.name}
+      </div>
+    </div>
+  });
+
+  
   return (
     <div>
-      {data.lenght}
+      <div className="flex flex-row justify-between m-3">
+        <h1 className="m-2 text-xl">Users</h1>
+        { isCreatingUser ? ( 
+          'Creating user...' 
+        ) : (
+          <Button onClick={ handleAddUser }>+ Add User</Button>
+        )}
+        {creatingUserError && 'Error creating User...'}
+      </div>
+      {renderedUsers}
     </div>
   )
 };
